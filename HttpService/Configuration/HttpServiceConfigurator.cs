ea2c129a0224a0363bdcdb2fb4a9b790cc4de128
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-06-03 21:04:40 +0430

    Fix : fix issue in registering IHttpClientFactory

--- HttpService/Configuration/HttpServiceConfigurator.cs
+++ HttpService/Configuration/HttpServiceConfigurator.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HttpService.Utils
+namespace HttpService.Configuration
 {
   public class HttpServiceConfigurator
   {
-    public static void Inject(IServiceCollection services)
+    public static void InjectHttpService(IServiceCollection services)
     {
+      services.AddHttpClient();
       services.AddTransient<IRequestService, RequestService>();
       services.AddTransient<IHttpService,Service.HttpService >();
     }
