53ac2377de9eada0cb2aa8b44e4ed7b1c12be9a3
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-07-14 08:47:39 +0430

    Feat : add more extension method for httpStatusCode
    IsRediredted , IsError , IsServerError are added and IsSuccessfull is refactored

--- HttpService/Utils/HttpStatusExtensions.cs
+++ HttpService/Utils/HttpStatusExtensions.cs
@@ -7,19 +7,15 @@ using System.Threading.Tasks;
 
 namespace HttpService.Utils;
 public static class HttpStatusExtensions
-  {
-    public static bool IsSuccessfulll(this HttpStatusCode httpStatusCode)
-      => httpStatusCode is HttpStatusCode.OK ||
-         httpStatusCode is HttpStatusCode.NoContent ||
-         httpStatusCode is HttpStatusCode.Created ||
-         httpStatusCode is HttpStatusCode.Accepted ||
-         httpStatusCode is HttpStatusCode.AlreadyReported ||
-         httpStatusCode is HttpStatusCode.IMUsed ||
-         httpStatusCode is HttpStatusCode.MultiStatus ||
-         httpStatusCode is HttpStatusCode.NonAuthoritativeInformation ||
-         httpStatusCode is HttpStatusCode.PartialContent ||
-         httpStatusCode is HttpStatusCode.ResetContent;
+{
+  public static bool IsSuccessfulll(this HttpStatusCode httpStatusCode)
+  => (int)httpStatusCode >= 200 && (int)httpStatusCode < 300;
+  public static bool IsRedirected(this HttpStatusCode httpStatusCode)
+  => (int)httpStatusCode >= 300 && (int)httpStatusCode < 400;
+  public static bool IsError(this HttpStatusCode httpStatusCode)
+  => (int) httpStatusCode >= 400 && (int) httpStatusCode < 400;
+  public static bool IsServerError(this HttpStatusCode httpStatusCode)
+  => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
 
-
-  }
+}
 
