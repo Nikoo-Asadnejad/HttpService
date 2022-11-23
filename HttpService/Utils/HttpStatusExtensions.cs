0212f5f6dafa1f89783921f9a6dfa7145c6999dc
Nikoo Asadnejad <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-11-23 14:19:19 +0330

    Feat(HttpStatusExtensions) : add extension methods for int statuses

--- HttpService/Utils/HttpStatusExtensions.cs
+++ HttpService/Utils/HttpStatusExtensions.cs
@@ -17,5 +17,13 @@ public static class HttpStatusExtensions
   public static bool IsServerError(this HttpStatusCode httpStatusCode)
   => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
 
+  public static bool IsSuccessfulll(this int httpStatusCode)
+ => (int)httpStatusCode >= 200 && (int)httpStatusCode < 300;
+  public static bool IsRedirected(this int httpStatusCode)
+  => (int)httpStatusCode >= 300 && (int)httpStatusCode < 400;
+  public static bool IsError(this int httpStatusCode)
+  => (int)httpStatusCode >= 400 && (int)httpStatusCode < 500;
+  public static bool IsServerError(this int httpStatusCode)
+  => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
 }
 
