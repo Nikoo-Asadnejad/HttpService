64558907001de8b0d05c48b05360a6838de8ed22
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-04-23 12:00:39 +0330

    Performance : replace newtonSoftJson with system.text.json

--- HttpService/Utils/JsonConverter.cs
+++ HttpService/Utils/JsonConverter.cs
@@ -1,18 +1,19 @@
-using Newtonsoft.Json;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HttpService.Utils;
 public static class JsonConverter
   {
     public static string Serialize<T>(this T obj)
-    => JsonConvert.SerializeObject(obj);
+    => JsonConverter.Serialize(obj);
 
     public static T Deserialize<T>(this string json)
-    => JsonConvert.DeserializeObject<T>(json);
+    => JsonConverter.Deserialize<T>(json);
 
   }
 
